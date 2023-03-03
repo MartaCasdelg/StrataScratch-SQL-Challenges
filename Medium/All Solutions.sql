87dc654d8723a8b16d30d9c9922e938f977e2146
MartaCasdelg <104106422+MartaCasdelg@users.noreply.github.com>
2023-03-03 11:37:20 +0000

    Update All Solutions.sql

--- Medium/All Solutions.sql
+++ Medium/All Solutions.sql
@@ -24,3 +24,23 @@ FROM
     title ON worker.worker_id = title.worker_ref_id
 WHERE
     worker.salary = (SELECT MAX(salary) FROM worker);
+
+
+-- Finding User Purchases
+WITH previous_purchases AS (
+    SELECT
+        user_id,
+        created_at,
+        LAG(created_at, 1) OVER (PARTITION BY user_id ORDER BY created_at) AS previous_purchase
+    FROM
+        amazon_transactions
+)
+
+SELECT DISTINCT
+    user_id
+FROM
+    previous_purchases
+WHERE
+    DATE_PART('day', created_at::timestamp - previous_purchase::timestamp) < 8
+ORDER BY
+    user_id;
