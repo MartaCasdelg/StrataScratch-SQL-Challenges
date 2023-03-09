5a36ac114a09ea263e28841e622b04bc10efc9e2
MartaCasdelg <104106422+MartaCasdelg@users.noreply.github.com>
2023-03-09 12:03:54 +0000

    Update All Solutions.sql

--- Hard/All Solutions.sql
+++ Hard/All Solutions.sql
@@ -6,3 +6,25 @@ FROM
     ts_stat('SELECT to_tsvector(contents) FROM google_file_store')
 WHERE
     word IN ('bull', 'bear');
+
+
+
+-- Monthly percentage difference
+WITH revenue_by_date AS (
+    SELECT
+        to_char(created_at, 'YYYY-MM') AS date,
+        SUM(value) AS revenue,
+        LAG(SUM(value), 1) OVER () as revenue_last_month
+    FROM
+        sf_transactions
+    GROUP BY
+        date
+    ORDER BY
+        date ASC
+)
+
+SELECT
+    date,
+    ROUND(((revenue - revenue_last_month)/revenue_last_month)*100, 2) AS revenue_diff_pct
+FROM
+    revenue_by_date;
