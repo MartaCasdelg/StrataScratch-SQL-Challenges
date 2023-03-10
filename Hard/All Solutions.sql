13aded02798e684d10b0bfd1c1d59544f87f2217
MartaCasdelg <104106422+MartaCasdelg@users.noreply.github.com>
2023-03-10 13:40:18 +0000

    Update All Solutions.sql

--- Hard/All Solutions.sql
+++ Hard/All Solutions.sql
@@ -28,3 +28,24 @@ SELECT
     ROUND(((revenue - revenue_last_month)/revenue_last_month)*100, 2) AS revenue_diff_pct
 FROM
     revenue_by_date;
+
+
+
+-- Premium vs freemium
+SELECT
+    d.date,
+    SUM(d.downloads) FILTER (WHERE a.paying_customer = 'no') AS non_paying_downloads,
+    SUM(d.downloads) FILTER (WHERE a.paying_customer = 'yes') AS paying_downloads
+FROM 
+    ms_download_facts d 
+    JOIN ms_user_dimension u 
+        ON d.user_id = u.user_id 
+    JOIN ms_acc_dimension a
+        ON u.acc_id = a.acc_id
+GROUP BY
+    d.date
+HAVING
+    SUM(d.downloads) FILTER (WHERE a.paying_customer = 'no') 
+        > SUM(d.downloads) FILTER (WHERE a.paying_customer = 'yes')
+ORDER BY
+    d.date;
